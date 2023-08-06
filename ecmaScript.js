0369833af4bd9646b3d3870106111df492381cb9
atikurahad <atikurahad@gmail.com>
2023-08-06 16:20:42 +0600

    rest perameter

--- ecmaScript.js
+++ ecmaScript.js
@@ -1,8 +1,18 @@
 
 
-function addTwoNumbers(num1,num2 , ...numbers){
-    let result = num1 + num2;
-    console.log(`${result}  ${numbers}`)
-}
+// function addTwoNumbers(num1,num2 , ...numbers){
+//     let result = num1 + num2;
+//     console.log(`${result}  ${numbers}`)
+// }
 
-addTwoNumbers(145,344,54,56,14,90)
+// addTwoNumbers(145,344,54,56,14,90)
+
+
+const arr1 = [12,34,11,45,65,9];
+const arr2 = [...arr1,"asc","wsd","dgefd","vhvhd"]
+
+  function add (){
+    return  arr2
+
+  }
+  console.log(add())
