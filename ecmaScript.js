ea8f5d5085cbda2c4491eb86eae5ec2e02af425a
atikurahad <atikurahad@gmail.com>
2023-08-09 15:14:21 +0600

    oop in scratch

--- ecmaScript.js
+++ ecmaScript.js
@@ -8,11 +8,22 @@
 // addTwoNumbers(145,344,54,56,14,90)
 
 
-const arr1 = [12,34,11,45,65,9];
-const arr2 = [...arr1,"asc","wsd","dgefd","vhvhd"]
+// const arr1 = [12,34,11,45,65,9];
+// const arr2 = [...arr1,"asc","wsd","dgefd","vhvhd"]
 
-  function add (){
-    return  arr2
+//   function add (){
+//     return arr2
 
-  }
-  console.log(add())
+//   }
+//   console.log(add())
+
+//  class player {
+//     constructor(name , age, profession){
+//         this.name = name,
+//       this.age = age,
+//         this.profession = profession
+//     }
+//  }
+
+//   const monir = new player("monir", 22 , " student");
+//   console.log(monir.profession)
